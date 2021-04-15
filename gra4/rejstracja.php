7b07e9b595ce910318096fa61732144eced32ac6
xFoReX <m.garbina@wp.pl>
2021-04-15 16:57:28 +0200

    mysqli baza rejstracja

--- gra4/rejstracja.php
+++ gra4/rejstracja.php
@@ -9,6 +9,35 @@
         integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 </head>
 <body>
+    <?php
+    if(isset($_REQUEST['action']) && isset($_REQUEST['email']) && isset($_REQUEST['password']))
+    {
+            $action = $_REQUEST['action'];
+            $email = $_REQUEST['email'];
+            $password = $_REQUEST['password'];
+
+            $db = new mysqli('localhost', 'root', '','registerandlogin');
+            if ($db->errno)
+            {
+                throw new RuntimeException('mysqli connection error: ' . $db->error);
+            }
+            
+            if($action == 'register')
+            {
+                $query = $db->prepare("INSERT INTO user (id, email, password) VALUES (NULL, ?, ?)");
+                $query->bind_param('ss', $email, $password);
+                $result = $query->execute();
+                if($result)
+                {
+                    echo "konto utworzono poprawnie.";
+                }
+                else
+                {
+                    echo "błąd podczas tworzenia konta";
+                }
+            }
+        }
+    ?>
     <div class="container">
         <div class="row">
             <div class="col-4 offset-4">
@@ -22,6 +51,7 @@
                 </form>
             </div>
         </div>
+        
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" 
         integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
